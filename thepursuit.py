f85fe5a9e610592335d7f07f49790cb819232dfa
AidanSpence <aidan.spence@student.onslow.school.nz>
2022-07-28 12:41:45 +1200

    big work

---
+++ thepursuit.py
@@ -0,0 +1,344 @@
+quick_questions = [["Which of these games includes the phrase “Do not pass go, do not collect $200”?","monopoly"],
+["The movie “Happy Feet” features what animals?","penguins"],
+['What sport is featured in the video game “FIFA”?',"football"],
+["Which mammal has no vocal cords?","giraffe"],
+["How many eyes does a bee have?","five"],
+["What was the verdict in the O.J. Simpson murder trial?","notguilty"],
+["What is evidence in court, where a person tells everything they saw or know?","testimony"],
+["Which planet has the most gravity?","jupiter"],
+["What was Superman’s birth name?","kalel"],
+["In what year was the Corvette introduced?","1953"],
+["What French ballet term means ‘in front’?","devant"],
+["Which actor appeared in films “Face Off” and “Ghost Rider”?","nicholascage"],
+["Which Williams sister has won more Grand Slam titles?","serena"],
+["What’s the primary ingredient in hummus?","chickpeas"],
+["Who wrote “Old Man and The Sea” and is considered one of the greatest writers of the 20thcentury?","ernesthemingway"],
+["Who was the messenger of the gods?","hermes"],
+["The American designer born in 1939 as Ralph Lifshitz is better known as what?","ralphlauren"],
+["Who did Forbes name the youngest self-made billionaire in 2019?","kyliejenner"],
+["Which continent is the largest?","asia"],
+["Which element is said to keep bones strong?","calcium"],
+["What does BMW stand for (in English)?","bavarianmotorworks"],
+["What is the name of the home of the Greek Gods?","olympus"],
+["Which kind of alcohol is Russia notoriously known for?","vodka"],
+["Which country did AC/DC originate in?","australia"],
+["What is the capital of New Zealand?","wellington"],
+["Which American president was involved in the Watergate scandal?","nixon"],
+["Donald Trump and which North Korean leader are known to trade threats over the internet?","kimjongun"],
+["During which Holiday is the Nutchracker primarily performed?","christmas"],
+["What was the name of the actor who played Jack Dawson in Titanic?","leonardodicaprio"],
+["Which popular TV show featured house Targaryen and Stark?","gameofthrones"],
+["What was the name of the family who starred in 7th Heaven?","camdens"],
+["How many Grammys does John Legend have?","ten"],
+["Which British girl group had a member by the name of Mel B?","spicegirls"],
+["Which song by Luis Fonsi and Daddy Yankee has the most views (of all time) on YouTube?","despacito"],
+["In what year was the first-ever Wimbledon Championship held?","1877"],
+["Which racer holds the record for the most Grand Prix wins?","michaelschumacher"],
+["Which is the only day of the week with nine letters in its name?","wednesday"],
+["By area, which is the largest county in England?","yorkshire"],
+["Auricular muscles are found in which part of the human body?","ear"],
+["Frank Skinner teamed up with which English comedian to write the lyrics to the song 'Three Lions'?","davidbaddiel"],
+["By population, which is the second largest city in Russia?","saintpetersburg"],
+["Which car maker produced the Focus, Mondeo and Fiesta models?","ford"],
+["Eric Theodore Cartman appears in which animated TV series?","southpark"],
+["What classic video game requires you to eat all the dots throughout a maze?","pacman"],
+["How many stripes does Adidas have?","3"],
+["What’s the fastest land animal in the world?","cheetah"],
+["What is the name of the song that Queen Elsa sings as she builds her ice castle in the movie Frozen?","letitgo"],
+["Which cartoon character lives in a pineapple under the sea?","spongebobsquarepants"]]
+
+multi_questions = [["What year was the very first model of the iPhone released?","2007","2001","2010",1],
+["What’s the shortcut for the “copy” function on most computers?","ctrl+shift c","ctrl c","alt c",2],
+["What is the smallest unit of memory?","Yobibits","kilobits","bits",3],
+["Who is often called the father of the computer?","John Mauchly","Charles Babbage","Howard Aiken",2],
+["What does “HTTP” stand for?","HyperText Transfer Protocol","HyperType Target Page","HyperTest The Protocals",1],
+["What is the name of the man who launched eBay back in 1995?","Jeff Bezos","Johnny Sins","Pierre Omidyar",3],
+["Which email service is owned by Microsoft?","Yahoo","Hotmail","Gmail",2],
+["Google Chrome, Safari, Firefox, and Explorer are different types of what?","Search Engines","Web browsers","Cringe",3],
+["What was Twitter’s original name?","Blue Bird","twttr","Tweet",2],
+["How many books are in the Catholic Bible?","Forty-two","Seventy-three","Sixty-seven",2],
+["How many fish were used to feed the 5,000 along with the loaves?","Two","Six","Three",1],
+["Which god is considered the “destroyer” and is part of the Hindu Trinity?","Brahma","Devi","Shiva",3],
+["The mother of the youngest Pandavas is?","Madri","Bhima","Nakula",1],
+["The Islamic calendar is based on which cycle?","Seasonal","Lunar","Solar",2],
+["Who was the last prophet in Islam?","Smith","Josephine","Muhammad",3],
+['“Cohen” is Hebrew for what?',"Priest","Recycle","Car",1],
+["What is the Hebrew term for a good deed done out of a sense of religious duty?","Caraty","Gazon","Mitzvah",3],
+["In which country was Buddha born?","Bangladesh","Nepal","France",2],
+["Where does Buddhism fall in a ranking of the world’s largest religion?","Fifth","Twelfth","Third",1],
+["What holiday is called “turkey day”?","Thanksgiving","Easter","Cristmas",1],
+["In the US, what month is Labor Day in?","October","March","September",3],
+["When is Canada Day?","October 21st","September 12th","July 1st",3],
+["Mexico’s Dia de los Muertos, means what in English?","Day of the Departed","Day of the Dead","Day of the Feast",2],
+["Oktoberfest in Germany is celebrated with what drink?","Beer","Coffee","Kinderpunsch",1],
+["What is celebrated on the 2nd Saturday in June in the United Kingdom?","The Queen's Official Birthday","The Annual Knife Fight","Good Friday",1],
+["Which country celebrates the Storming of the Bastille?","France","Estoina","Germany",1],
+["Mahatma Gandi’s birthday is a national holiday in which country?","Nepal","India","Laos",3],
+["The Chinese New Year is celebrated on what month?","March","January","May",3],
+["Independence Day in Cambodia is when?","4th July","12th December","9th November",3],
+["Where did backgammon originate?","Ottaman Empire","Persia","Czechoslovakia",3],
+["How many cards are there in a deck of Uno?","52","93","108",3],
+["In which board game do you try to solve a murder?","Trouble","Cluedo","Catan",2],
+["Which Street Fighter character wears a white outfit and a red headband?","Chun-Li","Ryu","Sagat",2],
+['“Astro Boy” is what genre of a video game?',"Action","Horror","RPG",1],
+["Who is the richest person in the world as of 2022?","Elon Musk","Jeff Bezos","Mark Zuckerburg",1],
+["Bill Gates is the founder of which company?","Apple","Microsoft","Windows",2],
+["In what year did Steve Jobs die?","2012","2016","2011",3],
+["Larry Page is the CEO of which company?","Google","Samsung","Dyson",1],
+["Richard Branson is the owner of which airline?","Delta Air","Quantas Air","Virgin Atlantic",3],
+["Which watch company has a pointed crown as its logo?","Omega","Rolex","Patek Philippe",2],
+["What is the slogan of Apple Inc.?","Think smart","Think big","Think different",3],
+["What is Hewlett Packard (HP) originally known for?","Printers","Washing machines","Computers",1],
+["How long is the gestation period of an African elephant?","22 months","8 months","19 months",1],
+["What’s the scientific name of a wolf?","Mustela nigripes","Panthera lupus","Canis lupus",3],
+["What is a female donkey called?","A Donket","A Donkey","A Jenny",3],
+["Which animal symbolizes good luck in Europe?","Cow","Ladybug","Rooster",2],
+["What name is used to refer to a group of frogs?","An army","A ribbit","A Following",1],
+["How many hearts does an octopus have?","Three","Four","One",1],
+["What is the name of the classification of crime which is less serious than a felony?","Misdemeanor","Pushover","Treason",1],
+["Who is the group of people sworn to make a decision/deliver a verdict in court?","Deciders","Jury","Monto",2],
+["How many homicides did Ted Bundy admit to?","16","24","30",3],
+["Ted Kaczynski was a former graduate of which university?","Portland","Harvard","Oxford",2],
+["Gregory Lee Johnson was convicted in Texas for what in 1989?","Murder","Tax evasion","Burning the flag",3],
+["Butch Cassidy was famous for doing what?","Giving to the Poor","Stealing","Wrestling",2],
+["How many presidents have been impeached?","Three","Four","Eight",1],
+["Before the laws were changed in 2000, how long was a French president’s term?","Seven years","Four years","Twelve Years",1],
+["Who has been the longest-serving monarch of the United Kingdom?","Queen Victoria","Queen Elizabeth II","King George III",2],
+["World War I began with the death of Archduke Franz Ferdinand, of which country?","Romania","Austria","Hungary",2],
+["What was Mao Zedong famous for?","Leading the Communist Party of China to victory during the Civil War","Creating the current calander system","Not needing to go to the bathroom",1],
+["Leader and political icon Nelson Mandela was from which African country?","South Africa","Namibia","Eygpt",1],
+["The largest political party in South Africa, the ANC, stands for what?","African Natural Company","African Nortical Congress","African National Congress",3],
+["What is the tallest building in the world?","Burj Khalifa","Empire state building","Auckland Skytower",1],
+["When did construction on the Empire State building started?","1927","1929","1866",2],
+["Who designed the Sydney Opera House?","Scott Morisson","Anton Gaudi","Jorn Utzon",3],
+["Between 1933 to 1947, the Hoover Dam was called what?","Gilbert Dam","Boulder Dam","Big Dam",2],
+["Where can you find the Petronas Towers?","Sao Paulo","Kuala Lumpur","Dhaka",2],
+["What’s another name for a footrest?","Ottoman","Stool","Salo",1],
+["Who discovered penicillin?","David Attenborough","Freya Gach","Alexander Fleming",3],
+["Who was the first woman to win a Nobel Prize?","Michelle Obama","Marie Curie","Mother Teresa",2],
+["What part of the atom has no electric charge?","Neutron","Proton","Electron",1],
+["What is the symbol for potassium?","Po","K","Sm",2],
+["What is meteorology the study of?","Meteors","Stars","Weather",3],
+["Which planet is the hottest in the solar system?","Venus","Sun","Mercury",1],
+["Which natural disaster is measured with a Richter scale?","Earthquakes","Tornados","Tsnami",1],
+["What animals are pearls found in?","Crabs","Lobsters","Oysters",3],
+["How many molecules of oxygen does ozone have?","Three","Eight","One",1],
+["What is Hawkeye’s real name?","Barry Smith","Clint Barton","Tony Stark",2],
+["Which Avenger is the only one who could calm the Hulk down?","Black Widow","Thor","Captian America",1],
+["Night Crawler, member of the X-Men, has what kind of powers?","Teleportation","Mind Control","ShapeShifting",1],
+["Which infinity stone was located on Vormir?","Soul Stone","Power Stone","Mind Stone",1],
+["What is the name of Batman’s butler?","Alfred","Gordon","Damian",1],
+["How many films did Sean Connery play James Bond in?","Four","Seven","Eight",2],
+["Which Indiana Jones movie was released back in 1984?","Indiana Jones and the Kingdom of the Crystal SKull","Raiders of The Lost Ark","Indian Jones and the Temple of Doom",2],
+["How many Lord of the Rings films are there?","Three","Four","One",1],
+["In what year was the first episode of South Park aired?","1997","1978","2001",1],
+["Who played Wolverine?","Johnny Depp","Daniel Radcliff","Hugh Jackman",3],
+["Which actress won the Oscar for Best Actress in 2003?","Scarlet Johansson","Jennifer Lawrence","Nicole Kidman",3],
+["The Young Victoria was played by which British actress?","Helen Mirren","Emily Blunt","Emma Watson",2],
+["How old was Coco Gauff when she first beat Venus Williams?","15","10","19",1],
+["In which year did Usain Bolt set his 100m in 9.58 seconds world record?","2009","2001","2004",1],
+["How many times had Rafael Nadal won the French Open by March 2020?","7","12","16",2],
+["When Michael Jordan played for the Chicago Bulls, how many NBA Championships did he win?","Six","Eight","Two",1],
+["In which US city is Broadway located?","Las Vagas","Los Angels","New York City",3],
+["What is the longest-running Broadway show?","The Phantom of the Opera","MJ","Aladdin",1],
+["Which show is the highest-grossing production on Broadway ever?","The Lion king","BeetleJuice","The Music Man",1],
+["In what year was Broadway established?","1950","1812","1750",3],
+["In which period did ballet originate?","Italian Renaisance","Golden Age","Information Age",1],
+["What is a plié in ballet?","Spinning","Jumping","Bending at the Knees",3],
+["Which animal can be seen on the Porsche logo?","Dog","Frog","Horse",3],
+["Which company owns Bugatti, Lamborghini. Audi, Porsche, and Ducati?","Volkswagen","Ford","Ferrari",1],
+["Which auto brand was the first to offer seat belts?","Volkswagen","Nash Motors","Detroit Automobile Company",1],
+["What or who is the Ford Mustang named after?","A political leader","A building","A fighter plane from WWII",3]]
+
+question_set_one = []
+question_set_two = []
+
+from random import randint
+import time
+loop = True
+choice = None
+FORMATTING = 20
+PLAY = "p"
+HELP = "h"
+QUIT = "q"
+TEST = "t"
+chaser_choice = None
+used_multi_question = []
+used_quick_question = []
+CHASER_BOARD_LOCATION = ["XXX"]
+PLAYER_BOARD_LOCATION = ["OOO"]
+EMPTY_BOARD_LOCATION = ["---"]
+board = [["MMM"],
+["---"],
+["---"],
+["---"],
+["---"],
+["---"],
+["---"],
+["---"],
+["WWW"]]
+chasers = [["The Wild Animal",60],["The Night Annihilator",69],["The Tutor", 45],["The Blood-stained", 96],["The Hazard", 74]]
+
+def menu(choice):
+    while choice is None:
+        print("""P: Play
+H: Help
+Q: Quit
+T: Test""")
+        choice = input("Enter a Choice: ").lower().strip()
+    print("=" * FORMATTING)
+    return choice
+
+def int_input(prompt):
+    while True:
+        try:
+            answer = int(input(prompt))
+            return answer
+        except ValueError as e:
+            print("Please enter a integer")
+
+def play():
+    minute = 60
+    start_time = time.time()
+    points = 0
+    money_choice = None
+    print("Welcome to the pursuit")
+    while True:
+        random_quick_question = randint(0,47)
+        while random_quick_question in used_quick_question:
+            random_quick_question = randint(0,47)
+        used_quick_question.append(random_quick_question)
+        print(quick_questions[random_quick_question][0])
+        print(f"Time: {minute - round(time.time() - start_time)} seconds")
+        user_answer = input("").lower().replace(" ","")
+        if time.time() - start_time > minute:
+            print("out of time")
+            break
+        if user_answer == quick_questions[random_quick_question][1]:
+            print("Correct")
+            points += 1
+        elif user_answer == "pass":
+            pass
+        else:
+            print("Incorrect")
+    print(points)
+
+    cash = points * 1000
+    player_location = None
+    while player_location is None:
+        print(f"""A: ${int(cash * 2)} 2 away from pursuer
+B: ${cash} 3 away from pursuer
+C: ${int(cash *0.5)} 4 away pursuer""")
+        money_choice = input("Enter a Choice: ").lower().strip()
+        if money_choice == "a":
+            cash = cash * 2.5
+            player_location = 3
+        elif money_choice == "b":
+            player_location = 4
+        elif money_choice == "c":
+            cash = cash * 0.5
+            player_location = 5
+        else:
+            print("no")
+    chaser_location = 1
+    board[chaser_location] = CHASER_BOARD_LOCATION
+    board[player_location] = PLAYER_BOARD_LOCATION
+    chaser_num = randint(0,4)
+    assinged_chaser = chasers[chaser_num][0]
+    print(f"Your pursuer is {assinged_chaser}")
+    for i in board:
+            print(i)
+    while player_location != 8:
+        if player_location == chaser_location:
+            print(f"you have been caught by {assinged_chaser}")
+            break
+        random_question = randint(0,100)
+        while random_question in used_multi_question:
+            random_question = randint(0,100)
+        used_multi_question.append(random_question)
+        print(multi_questions[random_question][0])
+        print(f"1. {multi_questions[random_question][1]}    2. {multi_questions[random_question][2]}    3. {multi_questions[random_question][3]}")
+        user_choice = int_input("")
+        if user_choice == multi_questions[random_question][4]:
+            print(f"Player Picked {user_choice}")
+            board[player_location] = EMPTY_BOARD_LOCATION
+            player_location += 1
+            board[player_location] = PLAYER_BOARD_LOCATION
+        else:
+            print(f"Player Picked {user_choice}")
+        chaser_luck = randint(0,100)
+        if chaser_luck <= chasers[chaser_num][1]:
+            chaser_choice = multi_questions[random_question][4]
+        else:
+            chaser_choice = randint(1,3)
+        if chaser_choice == multi_questions[random_question][4]:
+            print(f"{assinged_chaser} Picked {chaser_choice}")
+            board[chaser_location] = EMPTY_BOARD_LOCATION
+            chaser_location += 1
+            board[chaser_location] = CHASER_BOARD_LOCATION
+        else:
+            print(f"{assinged_chaser} Picked {chaser_choice}")
+        print(f"The correct answer was {multi_questions[random_question][4]}")
+        for i in board:
+            print(i)
+        
+
+def help():
+    option = None
+    repeat = True
+    while repeat:
+        print("""Q: Quick Question Help
+M: multiple Choice Help
+F: Final Pursuit Help
+E: Exit""")
+        option = input("Enter a Choice: ").lower().strip()
+        if option == "q":
+            print("""Quick Questions Help:
+You have 60 seconds to answer as many questions as you can
+Type 'pass' to skip a question""")
+        elif option == "m":
+            print(f"""Multiple Choice Questions Help:
+This is player location = {PLAYER_BOARD_LOCATION}
+This is pursuer location = {CHASER_BOARD_LOCATION}
+This is a empty space = {EMPTY_BOARD_LOCATION}
+This is your goal = ['WWW'] if you get to here without the
+purser catching you, you win the money if the purser catches you, you lose the money""")
+        elif option == "f":
+            print("""Final Pursuit Help:
+Just win""")
+        elif option == "e":
+            repeat = False
+        else:
+            print("Enter a valid choice ")
+
+def test():
+    cash = 9000
+    password = input("Enter Password ")
+    if password == "gaming":
+        print(f"""This is the final chase
+You have the chance to get away with ${cash}""")
+        print("A: For set A\nB: For set B")
+        chosen_set = input("").lower().strip()
+        if chosen_set == "a":
+            final_chase_questions = question_set_one
+            chaser_final_questions = question_set_two
+        elif chosen_set == "b":
+            final_chase_questions = question_set_two
+            chaser_final_question = question_set_one
+    else:
+        print("Incorrect Password")
+
+
+while loop:
+    choice = None
+    choice = menu(choice)
+    if choice == PLAY:
+        play()
+    elif choice == HELP:
+        help()
+    elif choice == QUIT:
+        loop = False
+    elif choice == TEST:
+        test()
+    else:
+        print("Enter a valid choice ")
